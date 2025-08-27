fbd05daeca4a2496e900891e83b966b1df4011f8
Aaron Cheng <acheng@ara.com>
2025-08-27 13:11:22 -0500

    rm lottery

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,11 +2,15 @@ import { helloCommand } from './commandFunctions/hello';
 import { createHelpCommand } from './commandFunctions/help';
 import { pingCommand } from './commandFunctions/ping';
 import { flipCommand } from './commandFunctions/flip';
-import { lotteryCommand } from './commandFunctions/lottery';
 import { Command } from './types';
 import { roastCommand } from './commandFunctions/roast';
 
-const _commands: Command[] = [helloCommand, pingCommand, flipCommand, lotteryCommand, roastCommand];
+const _commands: Command[] = [
+  helloCommand,
+  pingCommand,
+  flipCommand,
+  roastCommand,
+];
 const help = createHelpCommand(() => _commands);
 
 export const commands: Command[] = [..._commands, help];
